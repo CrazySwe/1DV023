32a6470c8898203879af52e954eee3c78e98dfee
Kevin Cederholm <kc222en@student.lnu.se>
2020-01-20 17:40:36 +0100

    Adding app.js

---
+++ the-node-platform-exercise-rotten-tomatoes/app.js
@@ -0,0 +1,10 @@
+/**
+ * @author Kevin Cederholm
+ * @version 1.0.0
+ */
+
+'use strict'
+
+const reviewer = require('./lib/reviewer.js')
+
+reviewer.printHelloWorld()
---
+++ the-node-platform-exercise-rotten-tomatoes/lib/reviewer.js
@@ -0,0 +1,10 @@
+/**
+ * @author Kevin Cederholm
+ * @version 1.0.0
+ */
+
+'use strict'
+
+module.exports.printHelloWorld = function () {
+  console.log('Hello World!')
+}
