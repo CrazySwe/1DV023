f4029e73cd60986b2283fa72f9bddb7dfa06dceb
Kevin Cederholm <kc222en@student.lnu.se>
2020-02-06 21:24:37 +0100

    Adding base for web scraper exercise

--- exercise-promising-web-scraper/app.js
+++ exercise-promising-web-scraper/app.js
@@ -1,1 +1,21 @@
-console.log('Hello World!')
+/**
+ * @author Kevin Cederholm
+ * @version 1.0.0
+ */
+
+'use strict'
+
+// What to require?
+const fse = require('fs-extra')
+
+// * Get the URLs from process.argv
+console.log('Process.')
+const varArr = process.argv.slice(2)
+
+
+// * Scrape/extract all absolute URLs for each URL provided
+console.log('Processing <URL>')
+
+// * write or append to file.json?
+
+fse.writeJSON('file.json', ['link1', 'link2', 'link3'], { encoding: 'utf8', spaces: ' ' })
---
+++ exercise-promising-web-scraper/lib/linkscraper.js
@@ -0,0 +1,13 @@
+/**
+ * Web scraper for URLs.
+ *
+ * @module lib/linkscraper.js
+ * @author Kevin Cederholm
+ * @version 1.0.0
+ */
+
+'use strict'
+
+// const scrapeLinks = async (urlArray) => something
+
+// module.exports.scrapeLinks = scrapeLinks
