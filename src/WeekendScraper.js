83fd8394a7b6f00119a798391419878171b49524
Kevin Cederholm <kc222en@student.lnu.se>
2020-02-08 17:37:20 +0100

    Adding base link scraping

--- src/WeekendScraper.js
+++ src/WeekendScraper.js
@@ -15,6 +15,7 @@ const Scraper = require('../lib/Scraper.js')
 const CalendarScraper = require('../lib/CalendarScraper.js')
 const CinemaScraper = require('../lib/CinemaScraper.js')
 const RestaurantScraper = require('../lib/RestaurantScraper.js')
+const cheerio = require('cheerio')
 
 class WeekendScraper extends Scraper {
   constructor (url = 'http://vhost3.lnu.se:20080/weekend') {
@@ -22,32 +23,42 @@ class WeekendScraper extends Scraper {
     // Initialize everything.
     this.mainUrl = url
     this.calendarScraper = url
-    // console.dir(url)
   }
 
   async run () {
     // Running scraper here.
     process.stdout.write('Scraping links...')
-    const html = await this.getHTMLPromise()
-    console.dir(html)
+    const html = await this.getHTMLPromise(this.url)
+    const page = cheerio.load(html)
+
+    // Might be a bad idea since css selection is case sensitive on :contains.
+    const calendarUrl = page('li > a:contains("Calendar")').attr('href')
+    const cinemaUrl = page('li > a:contains("cinema")').attr('href')
+    const restaurantUrl = page('li > a:contains("restaurant")').attr('href')
+    // else try to use this approach.
+    // page('li > a').each((index, element) => {
+    //   console.dir(element)
+    //   // console.dir(index)
+    // })
 
     // 1. Scrape links for Calendar, Cinema and Restaurant
     process.stdout.write('OK\n')
 
     // Use the 3 links to scrape rest.
-    const calScraper = new CalendarScraper('http://vhost3.lnu.se:20080/calendar/')
-    const cinScraper = new CinemaScraper('Cinema:url')
-    const restScraper = new RestaurantScraper('Restaurant:url')
+    const calScraper = new CalendarScraper(calendarUrl)
+    const cinScraper = new CinemaScraper(cinemaUrl)
+    const restScraper = new RestaurantScraper(restaurantUrl)
     //
     //
 
     process.stdout.write('Scraping available days...')
     // 2.
-    // const days = await calScraper.getDays()
+    // const days = await calScraper.getAvailableDays()
     process.stdout.write('OK\n')
 
     process.stdout.write('Scraping showtimes...')
     // 3.
+    // const times
     process.stdout.write('OK\n')
 
     process.stdout.write('Scraping possible reservations...')
