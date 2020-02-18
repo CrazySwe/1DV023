f485cb35c04bf8b141ea50b04203659e3103875b
Kevin Cederholm <kc222en@student.lnu.se>
2020-02-18 03:08:41 +0100

    Adding 404 and 500 error pages

--- app.js
+++ app.js
@@ -7,4 +7,31 @@
  */
 'use strict'
 
-console.log('Hello World!')
+const express = require('express')
+// const exphbs = require('express-hbs')
+// const path = require('path')
+
+const app = express()
+
+app.set('port', process.env.PORT || 8000)
+
+// Set the 404 error page
+app.use((err, req, res, next) => {
+  console.error(err)
+  res.type('text/plain')
+  res.status(404)
+  res.send('This is error 404 - Page not found.')
+})
+
+// Set the 500 error page
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.type('text/plain')
+  res.status(500)
+  res.send('This is error 500 - Internal Server Error.')
+})
+
+// Start listening
+app.listen(app.get('port'), () => {
+  console.log('Started on port ' + app.get('port') + ', press CTRL+C to terminate.')
+})
