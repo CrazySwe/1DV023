a8aa2142c405541575b01226024f52761ee00f2b
Kevin Cederholm <kc222en@student.lnu.se>
2020-02-25 16:28:09 +0100

    Updating snippet controller

--- controllers/snippetController.js
+++ controllers/snippetController.js
@@ -9,10 +9,12 @@ const SnippetModel = require('../models/snippet')
 const snippetController = {}
 
 snippetController.create = async (req, res) => {
+  // only users
   res.render('snippet/create', { title: 'Create New Snippet' })
 }
 
 snippetController.createPost = async (req, res) => {
+  // only users
   try {
     const snippet = new SnippetModel({
       title: 'testTitle',
@@ -22,12 +24,26 @@ snippetController.createPost = async (req, res) => {
     })
 
     await snippet.save()
-
-    res.redirect('/snippet/create')
   } catch (error) {
     console.error(error)
-    res.redirect('/snippet/create')
   }
+  res.redirect('/snippet/create')
+}
+
+snippetController.read = async (req, res) => {
+// Show the snippet PUBLIC
+}
+
+snippetController.update = async (req, res) => {
+// Show snippet to update and send post form
+}
+
+snippetController.updatePost = async (req, res) => {
+  // update snippet and show flash message
+}
+
+snippetController.delete = async (req, res) => {
+  // delete the snippet and show flash message
 }
 
 module.exports = snippetController
--- models/snippet.js
+++ models/snippet.js
@@ -10,12 +10,12 @@ const mongoose = require('mongoose')
 
 const snippetSchema = new mongoose.Schema({
   title: { type: String },
-  author: { type: String },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   body: { type: String },
   creationDate: { type: Date, default: Date.now },
   tags: [String]
 })
 
-const snippet = mongoose.model('snippet', snippetSchema)
+const snippet = mongoose.model('Snippet', snippetSchema)
 
 module.exports = snippet
